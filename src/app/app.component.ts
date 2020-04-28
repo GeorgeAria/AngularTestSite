be93b36ba4364e1dca2f64af69531c5aaab08b31
GeorgeAria <george.arias94@yahoo.com>
2020-04-27 23:29:02 -0700

    Added a new navLink to the Trade Shift page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
   selector: 'pm-root',
   //template should probably not be used. templateUrl may be more useful here
   template: `
-    <nav class='navbar navbar-expand navbar-light bg-light'>
+    <nav class='navbar navbar-expand navbar-dark bg-dark'>
         <a class='navbar-brand'>{{pageTitle}}</a>
         <ul class='nav nav-pills'>
           <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
           <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
+          <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/tradeShift']">Trade Shift</a></li>
         </ul>
     </nav>
     <div class='container'>
