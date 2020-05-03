f21ce682405e35ea0f7aadbbfbc8b2fd3fdd9028
GeorgeAria <george.arias94@yahoo.com>
2020-05-02 23:24:08 -0700

    Comments talk about how this is an interface and how it is used to create an object that holds the product data from our JSON file

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,3 +1,7 @@
+//The "IProduct" interface is used to represent a product object.
+//A collection of product objects can be found in the products.json file.
+//This interface stores the values located within the json file.
+
 export interface IProduct {
   productId: number;
   productName: string;
