1d462bb2d58ee52bb009710bf0dac20c15055f3f
GeorgeAria <george.arias94@yahoo.com>
2020-05-04 23:01:03 -0700

    Added code to subscribe to the user-data service and output JSON data in the web console

--- src/app/trade-shift/trade-shift.component.ts
+++ src/app/trade-shift/trade-shift.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from '../shared/user-data.service';
+import { User } from '../shared/user-data';
+
 
 @Component({
   templateUrl: './trade-shift.component.html',
@@ -6,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TradeShiftComponent implements OnInit {
 
-  constructor() { }
+  errorMessage = '';
+
+  users: User[] = [];
+
+  constructor(private userDataService: UserDataService) { }
 
   ngOnInit(): void {
+    this.userDataService.getUsers().subscribe({
+
+      //"next" is the function/key that will process the next emitted value.
+      //"products" is that emitted value.
+
+      next: users => {
+        this.users = users;
+      },
+
+      //"error" is the function/key that will process when an error occurs.
+
+      error: err => this.errorMessage = err
+    });
   }
 
 }
