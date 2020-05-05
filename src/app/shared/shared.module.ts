6d61af6ac22df15f17fb7cf623d77dff33b548cb
GeorgeAria <george.arias94@yahoo.com>
2020-05-04 22:57:26 -0700

    Comments added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 //FormsModule is imported so that the "ngModule" Angular directive can be used
+//CommonModule is needed in every feature module
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -12,6 +13,12 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule
   ],
+
+  //The "exports" array allows other modules to use this module's components when those modules import this module.
+  //System Angular modules can also be exported, like CommonModule and FormsModule.
+  //NOTE: Only export thing if another Angular module needs it.
+  //Also, never export a service (since it doesn't make sense given how they work).
+
   exports: [
     StarComponent,
     CommonModule,
