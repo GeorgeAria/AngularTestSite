61205663b3265a59f65447cce99c94eb17040a52
GeorgeAria <george.arias94@yahoo.com>
2020-05-04 22:56:30 -0700

    Comments added

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,8 +1,10 @@
 //This class is known as a "service".
 //A service is set up to be a single instance by Angular, which can be called upon by a class as a dependency.
 //This is called "Dependency Injection".
+
 //This class allows us to access our JSON data from our api/products folder.
 //In production, it would be used to get data from a backend service.
+//It may also be better to put these types of data services in their own Angular module.
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
@@ -24,25 +26,37 @@ export class ProductService {
   // because Stackblitz can't find the api folder.
   // private productUrl = 'assets/products/products.json';
 
+
   //"productUrl" is the url to the data. This can be an API (web url) if you have one.
 
   private productUrl = 'api/products/products.json';
 
-  //The constructor initializes an "HTTPClient" to help it communicate with a web server.
+  //The constructor initializes an "HTTPClient" to help it communicate with a web server or a JSON file.
 
   constructor(private http: HttpClient) { }
 
   //"getProducts" will return data from a JSON file.
   //It will only be called when it is subscribed to, which is shown in the "product-list.component.ts" file.
-  
+
   getProducts(): Observable<IProduct[]> {
+
+    console.log("Testing 1,2,3 ...");
     return this.http.get<IProduct[]>(this.productUrl)
+
+    //.pipe allows us to string together functional operators into a chain.
+
       .pipe(
+        
+    //.tap allows us to perform actions on data, but it can not modify it in any way.
+
         tap(data => console.log('All: ' + JSON.stringify(data))),
         catchError(this.handleError)
       );
   }
  
+  //"getProduct" will return a specific product based a an ID value that it is given.
+  //This is used by the "product-detail.component.ts" file to get a specific product.
+
   getProduct(id: number): Observable<IProduct | undefined> {
     return this.getProducts()
       .pipe(
