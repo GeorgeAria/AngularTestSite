148bfeb1442c9f54a75cddeccf5ae2112350fe29
GeorgeAria <george.arias94@yahoo.com>
2020-05-07 23:36:21 -0700

    Component file responsible for the functionality of the Trade-shift-day page

--- src/app/trade-shift/trade-shift.component.ts
+++ src/app/trade-shift/trade-shift.component.ts
@@ -13,26 +13,11 @@ export class TradeShiftComponent implements OnInit {
 
   errorMessage = '';
 
-  users: User[] = [];
   calendarData: CalendarData[] = [];
 
-  constructor(private userDataService: UserDataService, private calendarDataService: CalendarDataService) { }
+  constructor(private calendarDataService: CalendarDataService) { }
 
   ngOnInit(): void {
-    this.userDataService.getUsers().subscribe({
-
-      //"next" is the function/key that will process the next emitted value.
-      //"products" is that emitted value.
-
-      next: users => {
-        this.users = users;
-      },
-
-      //"error" is the function/key that will process when an error occurs.
-
-      error: err => this.errorMessage = err
-    });
-
     this.calendarDataService.getCalendarInfo().subscribe({
 
       //"next" is the function/key that will process the next emitted value.
